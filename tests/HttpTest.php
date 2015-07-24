6944094096938951690a29979ec2bf08dc549bd2
Jan Votava <votava@deployment.cz>
2015-07-24 17:06:30 +0200

    Add HTTP wrapper class

---
+++ tests/HttpTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use \P7\SSO\Http;
+use \GuzzleHttp\HandlerStack;
+use \GuzzleHttp\Middleware;
+use \GuzzleHttp\Handler\MockHandler;
+use \GuzzleHttp\Psr7\Response;
+
+class HttpTest extends PHPUnit_Framework_TestCase
+{
+  /**
+   * @vcr http_data_merge_get
+   */
+  public function testDataMergeGet() {
+    $client = new Http(['data' => ['foo' => 'bar']]);
+    $response = $client->get('http://httpbin.org/get', ['bar' => 'baz']);
+    $args = $response->data->args;
+
+    $this->assertEquals('bar', $args->foo);
+    $this->assertEquals('baz', $args->bar);
+  }
+
+  /**
+   * @vcr http_data_merge_post
+   */
+  public function testDataMergePostJSON() {
+    $client = new Http(['data' => ['foo' => 'bar']]);
+    $response = $client->post('http://httpbin.org/post', ['bar' => 'baz']);
+    $data = $response->data;
+
+    $this->assertEquals('{"foo":"bar","bar":"baz"}', $data);
+  }
+
+  /**
+   * @vcr http_base_uri_get
+   */
+  public function testWorksWithBaseUri() {
+    $client = new Http(['base_uri' => 'http://httpbin.org/']);
+    $response = $client->get('/get', ['foo' => 'bar']);
+    $data = $response->data;
+
+    $this->assertEquals('http://httpbin.org/get?foo=bar', $data->url);
+  }
+
+  /**
+   * @vcr http_get_auth_basic
+   */
+  public function testSendsAuthBasic() {
+    $client = new Http([
+      'auth' => ['pretty_nick', 'verysecretkey']
+    ]);
+    $response = $client->get('http://httpbin.org/get');
+    $headers = $response->data->headers;
+
+    $this->assertEquals('Basic cHJldHR5X25pY2s6dmVyeXNlY3JldGtleQ==', $headers->Authorization);
+  }
+
+  /**
+   * @vcr http_get_auth_bearer
+   */
+  public function testSendsAuthBearer() {
+    $client = new Http([
+      'headers' => [
+        'Authorization' => 'Bearer FOOBAR'
+      ]
+    ]);
+    $response = $client->get('http://httpbin.org/get');
+    $headers = $response->data->headers;
+
+    $this->assertEquals('Bearer FOOBAR', $headers->Authorization);
+  }
+}
