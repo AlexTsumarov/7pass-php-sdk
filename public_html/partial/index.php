09331a693581d4b8f0e8bb053489563d5f3c45ab
Jiri Pospisil <mekishizufu@gmail.com>
2015-09-29 18:33:52 +0200

    Reword the index page

--- public_html/partial/index.php
+++ public_html/partial/index.php
@@ -2,25 +2,30 @@
 
 <h2>
   7Pass PHP SDK Example Application
-  <div class="btn-group  pull-right" role="group">
+  <div class="btn-group pull-right" role="group">
     <a class="btn btn-default" href="https://github.com/p7s1-ctf/7pass-php-sdk" target="_blank" role="button">Documentation</a>
   </div>
 </h2>
 
 <p>
-  This sample application was developed to show you how 7Pass PHP SDK can be used to authenticate users on your site against 7Pass SSO provider and
-  how you can make 7Pass API calls on behalf this authenticated user subsequently.
+  The 7Pass PHP SDK library allows you to authenticate users against
+  the 7Pass SSO provider and provides utility methods for working with
+  the available user related APIs.
+
+  The purpose of the following example is to demonstrate the usage of
+  the library and show the interaction between the SDK and the 7Pass
+  SSO service.
 </p>
 
-<div class="well">
-  To obtain your 'client_id' and 'client_secret' keys please contact our 7Pass SSO team or Filip Skokan at <a href="mailto:filip.skokan@prosiebensat1.com?subject=7Pass Dev Account" target="_blank">filip.skokan@prosiebensat1.com</a>.
-</div>
+<p>
+  To use the library, you need to have a valid client. The client
+  represents the entity which is associated with a service you want to
+  authenticate to. To obtain the client credentials, you first need to
+  contact the 7Pass SSO team or contact Filip Skokan directly
+  at <a href="mailto:filip.skokan@prosiebensat1.com?subject=7Pass Dev
+  Account" target="_blank">filip.skokan@prosiebensat1.com</a>.
+</p>
 
-<?php if(!$loggedIn):?>
-  <p>
-    <b>To start the demonstration please use Login button above.</b>
-  </p>
-<?php endif?>
 
 <div class="panel panel-primary">
   <div class="panel-heading">
@@ -28,34 +33,47 @@
   </div>
   <div class="panel-body">
     <p>
-      In order to set up this SDK you pass a configuration which is specific just to your application.
-      This includes the following:
+      To use the library, it's necessary to initialize it with the
+      credentials of the client we want to use. If you don't have the
+      credentials yet, please see above.
+
       <ul>
         <li><b>client_id</b> (required)</li>
         <li><b>client_secret</b> (required)</li>
-        <li><b>environment</b> (optional) - defaults to: production; Available options: 'production', 'qa'</li>
       </ul>
     </p>
     <p>
-      We have got two environments running: 'production' and 'qa'.
-      'qa' environment is available for you for an intergration and testing purposes. Once you feel everything works as expected and
-      you are ready to live with your site, you just need to switch to 'production' environment providing production 'client_id' and 'client_secret'.
+      If you're starting the development, it's always a good idea to
+      work against a non live instance of the 7Pass SSO service. To
+      specify the instance (the environment) against which you want to
+      issue the requests, you can pass an additional key
+      called <b>environment</b> to the configuration. There're
+      currently two environments running: QA and production. Don't
+      forget to switch to the production version before you release
+      your application to the public.
     </p>
 <pre class="prettyprint">
 $config = [
   'client_id' => 'YOUR_CLIENT_ID',
   'client_secret' => 'YOUR_CLIENT_SECRET',
-  'environment' => 'qa' //default: 'production'
+  'environment' => 'qa' // Optional, defaults to 'production'
 ];
 
-//creates configuration
+// Creates the configuration object
 $ssoConfig = new P7\SSO\Configuration($config);
 
-//creates SSO object
+// Pass the configuration to the SSO object
 $sso = new P7\SSO($ssoConfig);
 </pre>
 
     </div>
   </div>
+
+<?php if(!$loggedIn):?>
+  <p>
+    Once the library is set up, we can proceed to authenticate a user.
+    <b>To start the demonstration, please use the Login button above.</b>
+  </p>
+<?php endif?>
 
 <?php require('partial/footer.php')?>
