dac8ea2486cc68b3fb1f8d632e17365037391af2
Matus Zeman <matus.zeman@gmail.com>
2015-10-02 09:31:51 +0200

    tests: vcr whitelist request matcher

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
 require __DIR__ . '/../lib/P7/SSO.php';
+
+$config = \VCR\VCR::configure();
+$config->enableRequestMatchers(array('method', 'url', 'host', 'query_string', 'body', 'post_fields'));
