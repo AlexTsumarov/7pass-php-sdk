6c7e58e2be043c3aa09da87291114c07061e2675
Alex <Alexandr_Tsumarov1@epam.com>
2016-01-06 02:04:21 +0300

    Update SSO.php
    Added compatibility with Symfony

--- lib/P7/SSO.php
+++ lib/P7/SSO.php
@@ -2,7 +2,8 @@
 
 namespace P7;
 
-require __DIR__ . '/../../vendor/autoload.php';
+// Add compatible to use with Symfony
+//require __DIR__ . '/../../vendor/autoload.php';
 
 use Firebase\JWT\JWT;
 use P7\SSO\Authorization;
